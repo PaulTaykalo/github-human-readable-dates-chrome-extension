e157194d98578f091d0cd11b08bc90779a6bf2d3
Paul Taykalo <tt.kilew@gmail.com>
2014-05-30 11:06:56 +0300

    Debounce added auto update each 0.250 ms on content change added

--- change-dates.js
+++ change-dates.js
@@ -1,8 +1,37 @@
-(function () {
+var updating_time_elements = false;
+
+var update_handler = 
+   $.debounce(250, 
+         function() {
+	          if (!updating_time_elements) {
+            	$('div.container').unbind("DOMSubtreeModified", update_handler); 
+	            update_date_time_elements();
+	           	$('div.container').bind("DOMSubtreeModified", update_handler);
+	          }
+         }
+    );
+
+$(document).ready(function () {
+   update_date_time_elements()
+   $('div.container').bind("DOMSubtreeModified", update_handler);
+});
+
+function update_date_time_elements() {
+  updating_time_elements = true;
+  var count = 0;
+
+  //console.log("Updating elements");
+  
   $("time").each(function(element) {
     $(this).text(moment($(this).attr("datetime")).fromNow());
+    count = count + 1;
   });
-  $("local-time").each(function(element) {
+   $("local-time").each(function(element) {
     $(this).text(moment($(this).attr("datetime")).fromNow());
   });
-})();
+
+  
+  //console.log("Updating elements completed (" + count + ")");
+
+  updating_time_elements = false;
+}
